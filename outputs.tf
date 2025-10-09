c5abd24ed19f4fe72f451f5e660c5168f88715dd
Triovians <connect@triovative.com>
2025-10-09 17:22:09 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -42,8 +42,3 @@ output "container_name" {
   value = azurerm_storage_container.state.name
 }
 
-output "primary_access_key" {
-  value     = azurerm_storage_account.tfstate.primary_access_key
-  sensitive = true
-}
-
