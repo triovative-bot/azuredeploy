71c0dd76e0d7eaad323e08c8f0afa3cca908fcb4
Triovians <connect@triovative.com>
2025-10-09 13:56:21 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,34 @@
+# Public IP of the Virtual Machine
 output "vm_public_ip" {
-  description = "Public IP address of the VM"
+  description = "The public IP address of the VM"
   value       = azurerm_public_ip.pip.ip_address
 }
 
-output "managed_identity_principal_id" {
-  description = "Principal ID of the Managed Identity"
-  value       = azurerm_windows_virtual_machine.vm.identity[0].principal_id
+# Private IP of the Virtual Machine
+output "vm_private_ip" {
+  description = "The private IP address of the VM"
+  value       = azurerm_network_interface.nic.private_ip_address
+}
+
+# User Assigned Managed Identity details
+output "managed_identity_id" {
+  description = "The ID of the User Assigned Managed Identity"
+  value       = azurerm_user_assigned_identity.uami.id
+}
+
+output "managed_identity_client_id" {
+  description = "The Client ID of the User Assigned Managed Identity"
+  value       = azurerm_user_assigned_identity.uami.client_id
+}
+
+# Resource Group name
+output "resource_group_name" {
+  description = "The Resource Group where resources are deployed"
+  value       = azurerm_resource_group.rg.name
+}
+
+# VM ID (for advanced automation or monitoring)
+output "vm_id" {
+  description = "The ID of the deployed Azure VM"
+  value       = azurerm_linux_virtual_machine.vm.id
 }
