50f947b3d34a73ad165447450c2698d181e0319f
Triovians <connect@triovative.com>
2025-10-09 16:30:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "${var.vm_name}-pip"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
