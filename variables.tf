90f37fd8a84f77850fc768b53594e06d2dae7cbe
Triovians <connect@triovative.com>
2025-10-09 13:42:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region to deploy resources"
   type        = string
-  default     = "East US"
+  default     = "Central India"
 }
 
 variable "vm_name" {
