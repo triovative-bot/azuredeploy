9d4c0cdc8e7ac1d548cac19c70213f7374e5e258
Triovians <connect@triovative.com>
2025-10-10 07:34:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,30 +20,6 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Create Storage Account
-resource "azurerm_storage_account" "tfstate" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  allow_blob_public_access = false
-  min_tls_version          = "TLS1_2"
-
-  lifecycle {
-    prevent_destroy = false
-  }
-
-}
-
-# Create a Blob Container for Terraform state
-resource "azurerm_storage_container" "state" {
-  name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
-}
-
 # Public IP
 resource "azurerm_public_ip" "pip" {
   name                = "${var.vm_name}-pip"
