09f213a20f9e8b8b2e49a19839c898bd5dc8a519
Triovians <connect@triovative.com>
2025-10-10 07:27:43 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,11 +10,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "tfstate-rg"
-    storage_account_name  = "tfstorage1"
-    container_name        = "tfstate"
-    key                   = "vm.terraform.tfstate"
-  }
-}
